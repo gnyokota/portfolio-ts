31fddf0f0a7bf37f30a4c223971040254acaeb0f
Giovana Yokota <gnyokota@gmail.com>
2021-06-19 21:44:33 +0200

    project web added

--- src/components/projects/ProjectItem.tsx
+++ src/components/projects/ProjectItem.tsx
@@ -1,33 +1,34 @@
-import React from "react";
+import Tilt from "react-parallax-tilt";
 
-const ProjectItem = () => {
+type Props = {
+  title: string;
+  description: string;
+  url: string;
+  code: string;
+  img: string;
+};
+
+const ProjectItem = ({ title, description, url, code, img }: Props) => {
   return (
-    <div>
-      <div className="project-wrapper__text">
-        <h3 className="project-wrapper__text-title">Project Title 0</h3>
-        <div>
-          <p className="mb-4">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi
-            neque, ipsa animi maiores repellendus distinctio aperiam earum dolor
-            voluptatum consequatur blanditiis inventore debitis fuga numquam
-            voluptate ex architecto itaque molestiae.
-          </p>
+    <div id="project">
+      <div className="project">
+        <h3 className="project__title">{title}</h3>
+        <div className="project__subtitle">
+          <p>{description}</p>
         </div>
-        <a target="_blank" className="cta-btn cta-btn--hero" href="#!">
+        <a href={url} className="project__live">
           See Live
         </a>
-        <a target="_blank" className="cta-btn text-color-main" href="#!">
+        <a href={code} className="project__code">
           Source Code
         </a>
       </div>
-      <div className="col-lg-8 col-sm-12">
-        <div className="project-wrapper__image">
-          <a href="#!" target="_blank">
-            <div data-tilt className="thumbnail rounded">
-              <img className="img-fluid" src="#" alt="project__img" />
-            </div>
+      <div className="project__image">
+        <Tilt>
+          <a href={url} className="project__image__link">
+            <img src={img} alt="project__img" />
           </a>
-        </div>
+        </Tilt>
       </div>
     </div>
   );
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,11 +1,24 @@
 import ProjectItem from "./ProjectItem";
+import project01 from "../../images/Project01.png";
 
 import "./Projects.scss";
 
 const Projects = () => {
   return (
     <div id="projects">
-      <ProjectItem />
+      <h1 className="projects__title">What I did until now</h1>
+      <p className="projects__subtitle">
+        Click to check the apps live and the source codes
+      </p>
+      <ProjectItem
+        title={"Pantanal ecommerce website"}
+        description={
+          "Integer vitae nisl odio. Ut in lacus in lacus mollis faucibus. Nulla egestas nec est quis lobortis. Proin eros metus, lacinia mattis porta in, mollis sit amet ipsum. Nunc vitae enim pellentesque, porta urna at, molestie est. Etiam condimentum lacinia condimentum. Phasellus vel ex velit. Pellentesque pharetra ex eu odio feugiat, vel sodales quam maximus. Donec a congue urna. Etiam tempor magna nisi, sed pulvinar mauris bibendum et. Donec ut vehicula ex. Mauris libero nisl, hendrerit sit amet sagittis ac, dignissim et lorem. Vestibulum sit amet gravida metus. Nam accumsan lacus quis volutpat tincidunt. Suspendisse scelerisque cursus iaculis."
+        }
+        url={"https://ecommerce-pantanal.netlify.app/"}
+        code={"https://github.com/gnyokota/frontend-ecommerce"}
+        img={project01}
+      />
     </div>
   );
 };
